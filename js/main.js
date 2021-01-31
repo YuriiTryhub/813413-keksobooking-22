0d4dd7d85c137b8431f5c55e313eb65f4d8d9ccf
YuriiTryhub <yuriitrihub@gmail.com>
2021-01-31 22:57:11 +0200

    настройка eslint/пример функции случайного числа от 1 до 3

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function randomInteger(min, max) {
+  // случайное число от min до (max+1)
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+alert( randomInteger(0, 3) ); //включая 0
+
+//взял из: https://learn.javascript.ru/task/random-int-min-max

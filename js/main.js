4c551ae80671f0f253ae5cb1429511a1ab8ea702
YuriiTryhub <yuriitrihub@gmail.com>
2021-01-31 23:43:07 +0200

    Добавление .toFixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 function randomInteger(min, max) {
   // случайное число от min до (max+1)
   let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+  return Math.floor(rand); // return rand.toFixed(4); - 4 знака после запятой.
 }
 
 alert( randomInteger(0, 3) ); //включая 0
+
 
 //взял из: https://learn.javascript.ru/task/random-int-min-max
